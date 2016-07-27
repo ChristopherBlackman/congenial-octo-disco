9ae0b1c163c6258aa0aff42f50dc7a80e86ce0b8
tyuijhgs <Mr.Christopher.Blackman@gmail.com>
2016-07-27 00:19:44 -0400

    basic design done

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+import time
 
 def getLinksImparative(url=""):
 	
@@ -9,7 +10,10 @@ def getLinksImparative(url=""):
 	pop = fifo_list.pop()
 	
 	while(pop != None):
-		print(str(pop))
+		fo = open("dump.txt", "a")
+		fo.write(str(pop)+ '\n')
+		fo.close()
+		print str(pop)
 		addNewLinksToQueueImparative(fifo_list,str(pop))
 		pop = fifo_list.pop()
 	
@@ -21,29 +25,35 @@ def addNewLinksToQueueImparative(fifo_list, parentLink = ""):
 	try:
 		response = requests.get(parentLink,timeout=(5))
 	except:
-		print "error - ", parentLink
+		print "<error> - ", parentLink
 		return
 	sourceCode = response.text
 	soup = BeautifulSoup(sourceCode,'html.parser')
 	links = soup.find_all('a')
 	for link in links:
-		newLink = checkLinkImparative(link, parentLink)
-		if(newLink != None):
-			fifo_list.add(newLink)
+		newLink = checkLinkImparative(link.get('href'), parentLink)
+		if(newLink == None):
+			continue
+		if not table.addUrl(str(newLink),parentLink):
+			continue
+		fifo_list.add(newLink)
 	return
 
 def checkLinkImparative(link, parent):
-	unicode_string = unicode(link.get('href'))
+	unicode_string = unicode(link)
 	normal_string = unicode_string.encode('utf-8')
-		
+	
+	#discard null strings (because people like doing this)
 	if len(normal_string) <= 0:
-		print 'string Less than equal to zero' , unicode_string
+		print '<caught>' , normal_string
 		return None
 	
+	#discard post requests
 	post = re.findall('\?.*$',normal_string)
 	if len(post) > 0:
 		return None	
 	
+	#discard static files
 	ending_url = re.findall('\.\w*$',normal_string)
 	if len(ending_url) > 0 and unwantedUrls(ending_url[0]):
 		return None
@@ -53,13 +63,13 @@ def checkLinkImparative(link, parent):
 		return None
 	#case for sorting out routing urls ('/') and ('//') urls (for some reason people add links like these)
 	elif normal_string[0] == '/':
-		stringObj = unicode_string.split('//',1)
+		stringObj = normal_string.split('//',1)
 		if len(stringObj) == 1:
 			return Node(parent + stringObj[0], parent)
 		elif len(stringObj) == 2:
 			return Node('https://' + stringObj[1], parent)
 	#default case (error prone here) needs to check if link is a link
-	return Node(unicode_string,parent)
+	return Node(normal_string,parent)
 
 def unwantedUrls(url):
 	#make it so that capitals and lowercases matter
@@ -69,6 +79,45 @@ def unwantedUrls(url):
 			return True
 	return False
 
+
+
+
+class HashTable:
+	def __init__(self,size=256):
+		self.table = [None]*size
+		self.size =size
+		
+	def __urlHash(self, url):
+		sum = 0
+		for letter in url:
+			sum += ord(letter)
+		remainder = sum % self.size
+		return remainder
+
+	def isUrlUsed(self, url):
+		node = self.table[self.__urlHash(url)]
+		while(node != None):
+			if(str(node) == url):
+				return True
+			node = node.get_next()
+		return False
+
+	def addUrl(self,url,parent_url):
+		hash_num = self.__urlHash(url)
+		node = self.table[hash_num]
+		lag = None
+		while(node != None):
+			if(str(node) == url):
+				return False
+			lag = node
+			node = node.get_next()
+		
+		if lag == None:
+			self.table[hash_num] = Node(url,parent_url)
+		else:
+			lag.set_next(Node(url,parent_url))
+		return True
+	
 class EnqueueList:
 	#create a node link list
 	#parameters(Node)
@@ -112,5 +161,6 @@ class Node:
 		return self.next
 	def get_parent(self):
 		return self.parent
-	
-getLinksImparative("https://en.wikipedia.org/wiki/Computer_science")
+
+table = HashTable(256)
+getLinksImparative("https://bheesham.com/")
--- test.py
+++ test.py
@@ -1,6 +1,18 @@
 import re
 
-a = " https://en.wikipedia.org/wiki/Computer_science/w/index.php?title=Computer_science&action=edit&section=1"
-post = re.findall('\?.*$',a)
+a = u"https://en.wikipedia.org/wiki/Computer_science"
+post = re.split("https://[a-zA-Z]{0,3}\.",a)
+dun = post[0].encode('utf-8')
 
-print post
+if(len(post) ==  2):
+	print post[1][0]
+else:
+	print post
+
+url = u"https://ja.wikipedia.org/wiki/%E3%83%A1%E3%82%A4%E3%83%B3%E3%83%9A%E3%83%BC%E3%82%B8"
+sum = 0
+for letter in url:
+	sum += ord(letter)
+
+average =int(sum/len(url))
+print average
